99db62543e8ed2f9a17fff03524ac2763ff4612c
ErantyInt <64794262+crtpi@users.noreply.github.com>
2020-05-22 09:00:54 -0700

    Update runcommand-onend.sh

--- runcommand-onend.sh
+++ runcommand-onend.sh
@@ -1,1 +1,7 @@
-if tvservice -s | grep NTSC; then tvservice -c "NTSC 4:3"; fbset -depth 8; fbset -depth 32; fi > /dev/null
+# when exiting an emulator -- revert to 320x240p
+echo 'tvservice -e "DMT 87"' > /opt/retropie/configs/all/desired_mode/value
+echo 'vcgencmd hdmi_timings 320 1 16 30 34 240 1 2 3 22 0 0 0 60 0 6400000 1 && tvservice -e "DMT 87" && fbset -depth 16 && fbset -depth 24 && fbset -depth 32 -xres 320 -yres 240' > /opt/retropie/configs/all/desired_mode/value #320x240p Timing
+vcgencmd hdmi_timings 320 1 16 30 34 240 1 2 3 22 0 0 0 60 0 6400000 1 > /dev/null
+tvservice -e "DMT 87" > /dev/null
+fbset -depth 16 && fbset -depth 24 && fbset -depth 32 -xres 320 -yres 240 > /dev/null
+echo 'runcommand-onend' > /opt/retropie/configs/all/desired_mode/value
